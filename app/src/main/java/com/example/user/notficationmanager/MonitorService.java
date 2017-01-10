783270a094b27e11925137b5e24ffa62397f79db
sppp <mail@sblo.heliohost.org>
2017-01-11 01:01:07 +0200

    Extending NotificationListenerService works now

---
+++ app/src/main/java/com/example/user/notficationmanager/MonitorService.java
@@ -0,0 +1,120 @@
+package com.example.user.notficationmanager;
+
+import java.util.ArrayList;
+import java.util.List;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.service.notification.NotificationListenerService;
+import android.service.notification.StatusBarNotification;
+import android.text.TextUtils;
+import android.util.Log;
+
+public class MonitorService extends NotificationListenerService {
+    private static final String TAG = "MonitorService";
+    private static final String TAG_PRE = "[" + MonitorService.class.getSimpleName() + "] ";
+    private static final int EVENT_UPDATE_CURRENT_NOS = 0;
+    public static final String ACTION_NLS_CONTROL = "com.example.user.notficationmanager.NLSCONTROL";
+    public static List<StatusBarNotification[]> current_notifications = new ArrayList<StatusBarNotification[]>();
+    public static int current_notification_count = 0;
+    public static StatusBarNotification posted_notification;
+    public static StatusBarNotification removed_notification;
+    private CancelNotificationReceiver receiver = new CancelNotificationReceiver();
+
+    private Handler monitor_handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case EVENT_UPDATE_CURRENT_NOS:
+                    UpdateCurrentNotifications();
+                    break;
+                default:
+                    break;
+            }
+        }
+    };
+
+    class CancelNotificationReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action;
+            if (intent != null && intent.getAction() != null) {
+                action = intent.getAction();
+                if (action.equals(ACTION_NLS_CONTROL)) {
+                    String command = intent.getStringExtra("command");
+                    if (TextUtils.equals(command, "cancel_last")) {
+                        if (current_notifications != null && current_notification_count >= 1) {
+                            StatusBarNotification sbnn = GetCurrentNotifications()[current_notification_count - 1];
+                            cancelNotification(sbnn.getPackageName(), sbnn.getTag(), sbnn.getId());
+                        }
+                    } else if (TextUtils.equals(command, "cancel_all")) {
+                        cancelAllNotifications();
+                    }
+                }
+            }
+        }
+
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(ACTION_NLS_CONTROL);
+        registerReceiver(receiver, filter);
+        monitor_handler.sendMessage(monitor_handler.obtainMessage(EVENT_UPDATE_CURRENT_NOS));
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(receiver);
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return super.onBind(intent);
+    }
+
+    @Override
+    public void onNotificationPosted(StatusBarNotification sbn) {
+        UpdateCurrentNotifications();
+        posted_notification = sbn;
+    }
+
+    @Override
+    public void onNotificationRemoved(StatusBarNotification sbn) {
+        UpdateCurrentNotifications();
+        removed_notification = sbn;
+    }
+
+    private void UpdateCurrentNotifications() {
+        try {
+            StatusBarNotification[] activeNos = getActiveNotifications();
+            if (current_notifications.size() == 0) {
+                current_notifications.add(null);
+            }
+            current_notifications.set(0, activeNos);
+            current_notification_count = activeNos.length;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static StatusBarNotification[] GetCurrentNotifications() {
+        if (current_notifications.size() == 0) {
+            return null;
+        }
+        return current_notifications.get(0);
+    }
+
+    private void LOG(Object object) {
+        Log.i(TAG, TAG_PRE+object);
+    }
+
+}
--- app/src/main/java/com/example/user/notficationmanager/NotificationManager.java
+++ app/src/main/java/com/example/user/notficationmanager/NotificationManager.java
@@ -1,84 +1,214 @@
 package com.example.user.notficationmanager;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.Gravity;
-import android.view.LayoutInflater;
-import android.view.MotionEvent;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.PopupWindow;
-import android.widget.RelativeLayout;
-//import android.app.NotificationManager;
-import android.content.BroadcastReceiver;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.os.Bundle;
-import android.util.Log;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.ComponentName;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.provider.Settings;
+import android.service.notification.StatusBarNotification;
 import android.support.v4.app.NotificationCompat;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
+import com.example.user.notficationmanager.R;
 
-public class NotificationManager extends AppCompatActivity {
+public class NotificationManager extends Activity {
 
-    private PopupWindow popupWindow;
-    private LayoutInflater layoutInflater;
-    private RelativeLayout relativeLayout;
+    private static final String TAG = "NotificationManager";
+    private static final String TAG_PRE = "["+NotificationManager.class.getSimpleName()+"] ";
+    private static final String ENABLED_LISTENERS = "enabled_notification_listeners";
+    private static final String ACTION_SETTINGS = "android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS";
+    private static final int EVENT_SHOW_CREATE = 0;
+    private static final int EVENT_LIST_CURRENT = 1;
+    private boolean is_enabled = false;
+    private TextView text_view;
 
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case EVENT_SHOW_CREATE:
+                    ShowCreateNotification();
+                    break;
+                case EVENT_LIST_CURRENT:
+                    ListCurrentNotification();
+                    break;
 
-    private NotificationReceiver not_recv;
+                default:
+                    break;
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // Start NotificaionListener service
-        startService(new Intent(NotificationManager.this, NotificationListener.class));
-
-        // Create NotificationReceiver
-        not_recv = new NotificationReceiver();
-        IntentFilter filter = new IntentFilter();
-        filter.addAction("com.example.user.NotificationManager");
-        registerReceiver(not_recv,filter);
-
-
-
-
-        Button btn = (Button)findViewById(R.id.create_notification);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                CreateTestNotification();
-            }
-        });
-
+        text_view = (TextView) findViewById(R.id.debug_log);
     }
 
-    void CreateTestNotification() {
-        android.app.NotificationManager nManager = (android.app.NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+    @Override
+    protected void onResume() {
+        super.onResume();
+        is_enabled = IsEnabled();
+        if (!is_enabled) {
+            ShowConfirmDialog();
+        }
+    }
+
+    public void buttonOnClicked(View view) {
+        text_view.setTextColor(Color.BLACK);
+        switch (view.getId()) {
+            case R.id.debug_create_notification:
+                DebugCreateNotification(this);
+                handler.sendMessageDelayed(handler.obtainMessage(EVENT_SHOW_CREATE), 50);
+                break;
+            case R.id.debug_cancel_last:
+                RemoveLastNotification();
+                handler.sendMessageDelayed(handler.obtainMessage(EVENT_LIST_CURRENT), 50);
+                break;
+            case R.id.debug_cancel_all:
+                ClearAllNotifications();
+                handler.sendMessageDelayed(handler.obtainMessage(EVENT_LIST_CURRENT), 50);
+                break;
+            case R.id.debug_refresh_log:
+                ListCurrentNotification();
+                break;
+            case R.id.debug_notification_settings:
+                OpenNotificationAccess();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private boolean IsEnabled() {
+        String name = getPackageName();
+        final String flat = Settings.Secure.getString(getContentResolver(),
+                ENABLED_LISTENERS);
+        if (!TextUtils.isEmpty(flat)) {
+            final String[] names = flat.split(":");
+            for (int i = 0; i < names.length; i++) {
+                final ComponentName cn = ComponentName.unflattenFromString(names[i]);
+                if (cn != null) {
+                    if (TextUtils.equals(name, cn.getPackageName())) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    private void DebugCreateNotification(Context context) {
+        android.app.NotificationManager nmgr = (android.app.NotificationManager) getSystemService(NOTIFICATION_SERVICE);
         NotificationCompat.Builder ncomp = new NotificationCompat.Builder(this);
         ncomp.setContentTitle("My Notification");
         ncomp.setContentText("Notification Listener Service Example");
         ncomp.setTicker("Notification Listener Service Example");
         ncomp.setSmallIcon(android.R.drawable.sym_def_app_icon);
         ncomp.setAutoCancel(true);
-        nManager.notify((int)System.currentTimeMillis(), ncomp.build());
-
-        Log.i("NotificationManager", "created notification");
+        nmgr.notify((int)System.currentTimeMillis(), ncomp.build());
     }
 
-    class NotificationReceiver extends BroadcastReceiver{
+    private void DebugCancelNotification(Context context, boolean is_cancel_all) {
+        Intent intent = new Intent();
+        intent.setAction(MonitorService.ACTION_NLS_CONTROL);
+        if (is_cancel_all) {
+            intent.putExtra("command", "cancel_all");
+        }else {
+            intent.putExtra("command", "cancel_last");
+        }
+        context.sendBroadcast(intent);
+    }
 
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            String temp = intent.getStringExtra("notification_event");
-            Log.i("NotificationManager", "NotificationReceiver.onReceive: " + temp);
+    private String GetCurrentNotificationString() {
+        String list = "";
+        StatusBarNotification[] current = MonitorService.GetCurrentNotifications();
+        if (current != null) {
+            for (int i = 0; i < current.length; i++) {
+                list = i +" " + current[i].getPackageName() + "\n" + list;
+            }
+        }
+        return list;
+    }
+
+    private void ListCurrentNotification() {
+        String result = "";
+        if (is_enabled) {
+            if (MonitorService.GetCurrentNotifications() == null)
+                return;
+            result = GetCurrentNotificationString();
+            text_view.setText(result);
+        }else {
+            text_view.setTextColor(Color.RED);
+            text_view.setText("Please Enable Notification Access");
         }
     }
+
+    private void RemoveLastNotification() {
+        if (is_enabled) {
+            DebugCancelNotification(this,false);
+        }else {
+            text_view.setTextColor(Color.RED);
+            text_view.setText("Please Enable Notification Access");
+        }
+    }
+
+    private void ClearAllNotifications() {
+        if (is_enabled) {
+            DebugCancelNotification(this,true);
+        }else {
+            text_view.setTextColor(Color.RED);
+            text_view.setText("Please Enable Notification Access");
+        }
+    }
+
+    private void ShowCreateNotification() {
+        if (MonitorService.posted_notification != null) {
+            String result = MonitorService.posted_notification.getPackageName()+"\n"
+                    + MonitorService.posted_notification.getTag()+"\n"
+                    + MonitorService.posted_notification.getId()+"\n"+"\n"
+                    + text_view.getText();
+            result = "Create notification:"+"\n"+result;
+            text_view.setText(result);
+        }
+    }
+
+    private void OpenNotificationAccess() {
+        startActivity(new Intent(ACTION_SETTINGS));
+    }
+
+    private void ShowConfirmDialog() {
+        new AlertDialog.Builder(this)
+                .setMessage("Please enable MonitorService access")
+                .setTitle("Notification Access")
+                .setIconAttribute(android.R.attr.alertDialogIcon)
+                .setCancelable(true)
+                .setPositiveButton(android.R.string.ok,
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                OpenNotificationAccess();
+                            }
+                        })
+                .setNegativeButton(android.R.string.cancel,
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                // do nothing
+                            }
+                        })
+                .create().show();
+    }
+
+    private void LOG(Object object) {
+        Log.i(TAG, TAG_PRE+object);
+    }
 }
-
-
-
